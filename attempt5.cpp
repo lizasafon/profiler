aa377f9637f529647676a3c76a4670e301bbbebd
lizasafon <safonova.ea@phystech.edu>
2020-11-25 20:41:29 +0300

    ready to test RBtree insert function graphs with help Red_Bla

--- attempt5.cpp
+++ attempt5.cpp
@@ -20,6 +20,12 @@ int random_generator(int min, int max)
 void insert(RedBlackTree &s, int obj){
 	s.insert(obj);
 }
+// set
+void insert_set(std::set<int> &s, int obj){
+	s.insert(obj);
+}
+
+
 void erase(std::set<float> &s, float obj){
 	s.erase(obj);
 }
@@ -31,6 +37,8 @@ void find(std::set<float> &s, float obj){
 //double expected(double n){
 //	return pow(10, -6)*(log(n*10)/50+10);
 //}
+
+
 
 double time_test(unsigned N){
 	std::vector<int> arr;
@@ -52,18 +60,39 @@ double time_test(unsigned N){
 	
 };
 
+//set
+
+double time_test_set(unsigned N){
+	std::vector<int> arr;
+	for(int i=0; i<N; i++){
+		arr.push_back(random_generator(-N, N));
+	}	
+	std::set<int>  s;
+
+	auto start = std::chrono::steady_clock::now();
+	for(int i=0; i<N; i++){
+		insert_set(s, arr[i]);// здесь будет одна из трех функций выше
+	}
+	auto end = std::chrono::steady_clock::now();
+
+
+ 
+	std::chrono::duration<double> time = end - start;
+	return time.count();
+	
+};
+
 int main(){
 	std::ofstream out;          // поток для записи
 	out.open("file.txt"); // окрываем файл для записи
 	if (out.is_open()){
 		srand(time(NULL));
 	        for(unsigned num=1; num<10000; num+=10){
-			//if(num%10==1)
-				//std::cout<< 100*float(num)/100 <<"%";
+
 	        	double time = time_test(num)/num;
-			//double expected_time = expected(static_cast<double>(num));
-			//out << num << '\t' << time << '\t' << expected_time << std::endl; //запись
-			out << num << '\t' << time << std::endl;
+			double expected_time = time_test_set(num)/num;
+			out << num << '\t' << time << '\t' << expected_time << std::endl; //запись
+			//out << num << '\t' << time << std::endl;
 		}
 	}
 	out.close();
