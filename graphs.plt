cdbc649d3ac6747be871c21fca671035e05fd1f1
lizasafon <safonova.ea@phystech.edu>
2020-11-25 20:56:49 +0300

    replaced previous files with one

--- graphs.plt
+++
@@ -1,6 +0,0 @@
-set term pdf
-set out 'map_insert.pdf'
-set xlabel "n"
-set ylabel "time"
-set title "map_insert"
-plot 'file.txt' using 1:2, 'file.txt' using 1:3
--- rbinsert.cpp
+++ rbinsert.cpp
@@ -5,18 +5,27 @@
 #include<ctime>
 #include<fstream>
 #include<cmath>
-//#include"tree.cpp"
+#include<random>
+#include<vector>
+#include"rbtree.cpp"
 
-float random_generator(int min, int max){
-	float n = float(min+rand()%(max-min)-1);
-	float k = float((rand()%1000))/1000;
-	return n+k;
+int random_generator(int min, int max)
+{
+	 std::random_device rd; 
+    std::mt19937 mersenne(rd());
+    int n = (min+mersenne()%(max-min)-1);
+    return n;
 }
 
-
-void insert(std::set<float> &s, float obj){
+void insert(RedBlackTree &s, int obj){
 	s.insert(obj);
 }
+// set
+void insert_set(std::set<int> &s, int obj){
+	s.insert(obj);
+}
+
+
 void erase(std::set<float> &s, float obj){
 	s.erase(obj);
 }
@@ -25,16 +34,18 @@ void find(std::set<float> &s, float obj){
 } 
 
 
-double expected(double n){
-	return pow(10, -6)*(n*log(n*10)/50+10);
-}
+//double expected(double n){
+//	return pow(10, -6)*(log(n*10)/50+10);
+//}
+
+
 
 double time_test(unsigned N){
-	float* arr = new float[N];
+	std::vector<int> arr;
 	for(int i=0; i<N; i++){
-		arr[i] = random_generator(-N, N);
+		arr.push_back(random_generator(-N, N));
 	}	
-	std::set<float>  s;
+	RedBlackTree  s;
 
 	auto start = std::chrono::steady_clock::now();
 	for(int i=0; i<N; i++){
@@ -42,9 +53,29 @@ double time_test(unsigned N){
 	}
 	auto end = std::chrono::steady_clock::now();
 
-	delete[] arr;
 
-	//delete[] s;
+ 
+	std::chrono::duration<double> time = end - start;
+	return time.count();
+	
+};
+
+//set
+
+double time_test_set(unsigned N){
+	std::vector<int> arr;
+	for(int i=0; i<N; i++){
+		arr.push_back(random_generator(-N, N));
+	}	
+	std::set<int>  s;
+
+	auto start = std::chrono::steady_clock::now();
+	for(int i=0; i<N; i++){
+		insert_set(s, arr[i]);// здесь будет одна из трех функций выше
+	}
+	auto end = std::chrono::steady_clock::now();
+
+
  
 	std::chrono::duration<double> time = end - start;
 	return time.count();
@@ -57,11 +88,11 @@ int main(){
 	if (out.is_open()){
 		srand(time(NULL));
 	        for(unsigned num=1; num<10000; num+=10){
-			if(num%1000==1)
-				std::cout<< 100*float(num)/10000 <<"%";
-	        	double time = time_test(num);
-			double expected_time = expected(static_cast<double>(num));
+
+	        	double time = time_test(num)/num;
+			double expected_time = time_test_set(num)/num;
 			out << num << '\t' << time << '\t' << expected_time << std::endl; //запись
+			//out << num << '\t' << time << std::endl;
 		}
 	}
 	out.close();
@@ -71,5 +102,4 @@ int main(){
 	//std::cout << "N = " << num << '\t' << "elapsed time: " << time << 's' <<'\n';
 	//std::cout << random_generator(-200, 200) << '\n';
 	}
-
 
