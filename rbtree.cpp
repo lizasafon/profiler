705257fd282b9b5d74dbf460ddd1c3bcf4143443
Anastasia Kliushnik <kliushnik.av@phystech.edu>
2020-11-30 03:24:20 +0300

    RBtree working without parent tree (with template)

--- rbtree.cpp
+++ rbtree.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-#include <tree.cpp>
 using namespace std;
 
-uzel* grandparent(uzel* point) {
+template <class Type>
+struct uzel {
+    Type key;
+    uzel<Type>* left;
+    uzel<Type>* right;
+    uzel<Type>* parent;
+    bool color; //true - RED, false - BLACK
+    uzel(Type k) {
+        key = k;
+        left = NULL;
+        right = NULL;
+        parent = NULL;
+        color = true;
+    }
+
+};
+template <class Type>
+uzel<Type>* grandparent(uzel<Type>* point) {
     if ((point != NULL) && (point->parent != NULL)) {
         return point->parent->parent;
     }
@@ -10,9 +26,9 @@ uzel* grandparent(uzel* point) {
         return NULL;
     }
 }
-
-uzel* uncle(uzel* point) {
-    uzel* g = grandparent(point);
+template <class Type>
+uzel<Type>* uncle(uzel<Type>* point) {
+    uzel<Type>* g = grandparent(point);
     if (g == NULL) {
         return NULL;
     }
@@ -23,8 +39,8 @@ uzel* uncle(uzel* point) {
         return g->left;
     }
 }
-
-uzel* brother(uzel* point) {
+template <class Type>
+uzel<Type>* brother(uzel<Type>* point) {
     if (point == point->parent->left) {
         return point->parent->right;
     }
@@ -32,31 +48,66 @@ uzel* brother(uzel* point) {
         return point->parent->left;
     }
 }
+template <class Type>
+bool iscolor(uzel<Type>* point) {
+    if (point != NULL) {
+        return point->color;
+    }
+    else {
+        return false;
+    }
+}
 
-class RedBlackTree : public BinSearchTree {
+
+template <class Type>
+class RedBlackTree {
     public:
-        void insert(int numb) {
+        uzel<Type>* start;
+        RedBlackTree() {
+            start = NULL;
+        }
+        void insert(Type numb) {
             if (start != NULL) {
                 insert_it(numb, start);
             }
             else {
-                start = new uzel(numb);
+                start = new uzel<Type>(numb);
+                start->color = false;
             }
         }
-        void erase(int numb) {
-            erase_it(numb, start);
+        void erase(Type numb) {
+            erase_it(find(numb));
+        }
+        uzel<Type>* find(Type numb) {
+            return find_it(numb, start);
         }
     private:
+//searching function
+        uzel<Type>* find_it(Type numb, uzel<Type>* point) {
+            if (point != NULL) {
+                if (numb == point->key) {
+                    return point;
+                }
+                else if (numb < point->key) {
+                    return find_it(numb, point->left);
+                }
+                else if (numb > point->key) {
+                    return find_it(numb, point->right);
+                }
+                else {
+                    return NULL;
+                }
+            }
+        }
 //insert function
-        void insert_it(int numb, uzel* point) {
+        void insert_it(Type numb, uzel<Type>* point) {
             if (numb < point->key) {
                 if (point->left != NULL) {
                     insert_it(numb, point->left);
                 }
                 else {
-                    point->left = new uzel(numb);
+                    point->left = new uzel<Type>(numb);
                     point->left->parent = point;
-                    point->left->color = true;
                     balance_insert(point->left);
                 }
             }
@@ -65,72 +116,77 @@ class RedBlackTree : public BinSearchTree {
                     insert_it(numb, point->right);
                 }
                 else {
-                    point->right = new uzel(numb);
+                    point->right = new uzel<Type>(numb);
                     point->right->parent = point;
-                    point->right->color = true;
                     balance_insert(point->right);
                 }
             }
         }
 //erase function
-        void erase_it(int numb, uzel* point) {
+        void erase_it(uzel<Type>* point) {
+            uzel<Type>* x;
+            uzel<Type>* y;
             if (point == NULL) {
                 return;
             }
+            if ((point->left == NULL) || (point->right == NULL)) {
+                    y = point;
+                }
             else {
-                if (numb < point->key) {
-                    erase_it(numb, point->left);
+                y = find_min(point->right);
+            }
+            if (!((point->left == NULL) && (point->right == NULL))) {
+                if (y->left != NULL) {
+                    x = y->left;
                 }
-                else if (numb > point->key) {
-                    erase_it(numb, point->right);
+                else {
+                    x = y->right;
                 }
-                else if (numb == point->key) {
-                    if ((point->left == NULL) && (point->right == NULL)) {
-                        if (point->parent->left == point) {
-                            point->parent->left = NULL;
-                        }
-                        else {
-                            point->parent->right = NULL;
-                        }
-                        uzel* u = point->parent;
-                        delete point;
-                        balance_delete(u);
-                    }
-                    else if ((point->left == NULL) && (point->right != NULL)) {
-                        point->left = point->right->left;
-                        point->right = point->right->right;
-                        point->key = point->right->key;
-                        point->right->left->parent = point;
-                        point->right->right->parent = point;
-                        delete point->right;
-                        balance_delete(point);
-                    }
-                    else if ((point->left != NULL) && (point->right == NULL)) {
-                        point->left = point->left->left;
-                        point->right = point->left->right;
-                        point->key = point->left->key;
-                        point->left->left->parent = point;
-                        point->left->right->parent = point;
-                        delete point->left;
-                        balance_delete(point);
+                x->parent = y->parent;
+                if (y->parent != NULL) {
+                    if (y == y->parent->left) {
+                        y->parent->left = x;
                     }
                     else {
-                        uzel* u = find_min(point->right);
-                        point->key = u->key;
-                        erase_it(u->key, u);
+                        y->parent->right = x;
                     }
+                }
+                else {
+                    start = x;
+                }
+                if (y != point) {
+                    point->key = y->key;
+                }
+                if (y->color == false) {
+                    balance_delete(x);
+                }
+                delete y;
+            }
+            else {
+                if (y->parent != NULL) {
+                    x = y->parent;
+                    if (y == x->left) {
+                        x->left = NULL;
+                    }
+                    else {
+                        x->right = NULL;
+                    }
+                }
+                else {
+                    delete y;
+                    start = NULL;
                 }
             }
         }
 //balance functions
-        void balance_insert(uzel* point) {
+        void balance_insert(uzel<Type>* point) {
             insert_1(point);
         }
-        void balance_delete(uzel* point) {
+        void balance_delete(uzel<Type>* point) {
             delete_1(point);
         }
 //find minimum
-        uzel* find_min(uzel* point) {
+        uzel<Type>* find_min(uzel<Type>* point) {
             if (point->left == NULL) {
                 return point;
             }
@@ -139,44 +195,58 @@ class RedBlackTree : public BinSearchTree {
             }
         }
 //tree turns
-        void turn_left(uzel* point) {
-            uzel* n = point->right;
-            n->parent = point->parent;
+        void turn_left(uzel<Type>* point) {
+            uzel<Type>* t = point->right;
+            point->right = t->left;
+            if (t->left != NULL) {
+                t->left->parent = point;
+            }
+            if (t != NULL) {
+                t->parent = point->parent;
+            }
             if (point->parent != NULL) {
                 if (point == point->parent->left) {
-                    point->parent->left = n;
+                    point->parent->left = t;
                 }
                 else {
-                    point->parent->right = n;
+                    point->parent->right = t;
                 }
             }
-            point->right = n->left;
-            if (n->left != NULL) {
-                n->left->parent = point;
+            else {
+                start = t;
             }
-            point->parent = n;
-            n->left = point;
+            t->left = point;
+            if (point != NULL) {
+                point->parent = t;
+            }
         }
-        void turn_right(uzel* point) {
-            uzel* n = point->left;
-            n->parent = point->parent;
+        void turn_right(uzel<Type>* point) {
+            uzel<Type>* t = point->left;
+            point->left = t->right;
+            if (t->right != NULL) {
+                t->right->parent = point;
+            }
+            if (t != NULL) {
+                t->parent = point->parent;
+            }
             if (point->parent != NULL) {
-                if (point == point->parent->left) {
-                    point->parent->left = n;
+                if (point == point->parent->right) {
+                    point->parent->right = t;
                 }
                 else {
-                    point->parent->right = n;
+                    point->parent->left = t;
                 }
             }
-            point->left = n->right;
-            if (n->right != NULL) {
-                n->right->parent = point;
+            else {
+                start = t;
             }
-            point->parent = n;
-            n->right = point;
+            t->right = point;
+            if (point != NULL) {
+                point->parent = t;
+            }
         }
 //cases for insert
-        void insert_1(uzel* point) {
+        void insert_1(uzel<Type>* point) {
             if (point->parent == NULL) {
                 point->color = false;
             }
@@ -184,7 +254,7 @@ class RedBlackTree : public BinSearchTree {
                 insert_2(point);
             }
         }
-        void insert_2(uzel* point) {
+        void insert_2(uzel<Type>* point) {
             if (point->parent->color == false) {
                 return;
             }
@@ -192,21 +262,26 @@ class RedBlackTree : public BinSearchTree {
                 insert_3(point);
             }
         }
-        void insert_3(uzel* point) {
-            uzel* u = uncle(point);
-            if ((u->color == true) && (u != NULL)) {
-                point->parent->color = false;
-                u->color = false;
-                uzel* g = grandparent(point);
-                g->color = true;
-                insert_1(g);
+        void insert_3(uzel<Type>* point) {
+            uzel<Type>* u = uncle(point);
+            if (u != NULL) {
+                if (u->color == true) {
+                    point->parent->color = false;
+                    u->color = false;
+                    uzel<Type>* g = grandparent(point);
+                    g->color = true;
+                    insert_1(g);
+                }
+                else {
+                    insert_4(point);
+                }
             }
             else {
                 insert_4(point);
             }
         }
-        void insert_4(uzel* point) {
-            uzel* g = grandparent(point);
+        void insert_4(uzel<Type>* point) {
+            uzel<Type>* g = grandparent(point);
             if ((point == point->parent->right) && (point->parent == g->left)) {
                 turn_left(point->parent);
                 point = point->left;
@@ -217,8 +292,8 @@ class RedBlackTree : public BinSearchTree {
             }
             insert_5(point);
         }
-        void insert_5(uzel* point) {
-            uzel* g = grandparent(point);
+        void insert_5(uzel<Type>* point) {
+            uzel<Type>* g = grandparent(point);
             point->parent->color = false;
             g->color = true;
             if ((point == point->parent->left) && (point->parent == g->left)) {
@@ -229,16 +304,16 @@ class RedBlackTree : public BinSearchTree {
             }
         }
 //cases for delete
-        void delete_1(uzel* point) {
+        void delete_1(uzel<Type>* point) {
             if(point->parent != NULL) {
                 delete_2(point);
             }
         }
-        void delete_2(uzel* point) {
-            uzel* b = brother(point);
-            if(b->color == true) {
+        void delete_2(uzel<Type>* point) {
+            uzel<Type>* b = brother(point);
+            if(iscolor(b) == true) {
                 point->parent->color = true;
-                b->color = false;
+                if (b != NULL) {b->color = false;}
                 if (point == point->parent->left) {
                     turn_left(point->parent);
                 }
@@ -248,52 +323,52 @@ class RedBlackTree : public BinSearchTree {
             }
             delete_3(point);
         }
-        void delete_3(uzel* point) {
-            uzel* b = brother(point);
-            if((point->parent->color == false) && (b->color = false) && (b->left->color = false) && (b->right->color = false)) {
-                b->color = true;
+        void delete_3(uzel<Type>* point) {
+            uzel<Type>* b = brother(point);
+            if((iscolor(point->parent) == false) && (iscolor(b) == false) && (iscolor(b->left) == false) && (iscolor(b->right) == false)) {
+                if (b != NULL) {b->color = true;}
                 delete_1(point->parent);
             }
             else {
                 delete_4(point);
             }
         }
-        void delete_4(uzel* point) {
-            uzel* b = brother(point);
-            if((point->parent->color == true) && (b->color = false) && (b->left->color = false) && (b->right->color = false)) {
-                b->color = true;
+        void delete_4(uzel<Type>* point) {
+            uzel<Type>* b = brother(point);
+            if((iscolor(point->parent) == true) && (iscolor(b) == false) && (iscolor(b->left) == false) && (iscolor(b->right) == false)) {
+                if (b != NULL) {b->color = true;}
                 point->parent->color = false;
             }
             else {
                 delete_5(point);
             }
         }
-        void delete_5(uzel* point) {
-            uzel* b = brother(point);
+        void delete_5(uzel<Type>* point) {
+            uzel<Type>* b = brother(point);
             if(b->color = false) {
-                if ((point->parent->left == point) && (b->left->color = true) && (b->right->color = false)) {
-                    b->color = true;
-                    b->left->color = false;
+                if ((point->parent->left == point) && (iscolor(b->left) == true) && (iscolor(b->right) == false)) {
+                    if (b != NULL) {b->color = true;}
+                    if ((b != NULL) && (b->left != NULL)) {b->left->color = false;}
                     turn_right(b);
                 }
-                else if ((point->parent->right == point) && (b->left->color = false) && (b->right->color = true)) {
-                    b->color = true;
-                    b->right->color = false;
+                else if ((point->parent->right == point) && (iscolor(b->left) == false) && (iscolor(b->right) == true)) {
+                    if (b != NULL) {b->color = true;}
+                    if ((b != NULL) && (b->right != NULL)) {b->right->color = false;}
                     turn_left(b);
                 }
             }
             delete_6(point);
         }
-        void delete_6(uzel* point) {
-            uzel* b = brother(point);
-            b->color = point->parent->color;
+        void delete_6(uzel<Type>* point) {
+            uzel<Type>* b = brother(point);
+            if (b != NULL) {b->color = point->parent->color;}
             point->parent->color = false;
             if(point == point->parent->left) {
-                b->right->color = false;
+                if ((b != NULL) && (b->right != NULL)) {b->right->color = false;}
                 turn_left(point->parent);
             }
             else {
-                b->left->color = true;
+                if ((b != NULL) && (b->left != NULL)) {b->left->color = true;}
                 turn_right(point->parent);
             }
         }
